3e27302509e184f3104ced2e37b6ad43a0b2ee0c
andre <antonhalki@gmail.com>
2022-11-03 17:27:16 +0300

    cum

--- src/main/java/dev/Andorvini/Main.java
+++ src/main/java/dev/Andorvini/Main.java
@@ -3,16 +3,12 @@ package dev.Andorvini;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.*;
+import com.pengrad.telegrambot.model.request.*;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,42 +19,60 @@ public class Main {
         HashMap<Long, Boolean> userState = new HashMap();
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:D:\\JavaProjects\\MechanicsBot\\src\\main\\java\\dev\\Andorvini\\test.db");
-            Statement statement = connection.createStatement();
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:test.db");
 
             bot.setUpdatesListener(updates -> {
 
-
                 for (Update update: updates) {
 
-                    long chatId = update.message().chat().id();
+                    InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                        new InlineKeyboardButton("cum").callbackData("cum"));
+
+//                      if (update.message() != null) {
+//                          long chatId = update.message().chat().id();
+//                      } else if (update.callbackQuery() != null) {
+//                          long chatId = update.callbackQuery().
+//                      }
+
+                    long chatId = 483452411;
 
                     if (!userState.containsKey(chatId)) {
                         userState.put(chatId,false);
                     }
 
-                    if (Objects.equals(update.message().text(), "/tablecreate")) {
+                    if  (!userState.get(chatId)) {
 
-                        if (!userState.get(chatId)) {
-                            SendResponse tableNameResponse = bot.execute(new SendMessage(chatId,"Enter table name"));
-                            userState.put(chatId,true);
-                        } else if (userState.get(chatId)) {
-                            String tableName = update.message().text();
-                            try {
-                                statement.executeUpdate("CREATE TABLE" + tableName + "(cum INTEGER);");
-                            } catch (Exception e) {
-                                System.out.println(e.getMessage());
+                        if (update.message() != null) {
+                            if (update.message().text().equals("/tablecreate")) {
+                                SendResponse tableNameResponse = bot.execute(new SendMessage(chatId, "Enter table name").replyMarkup(inlineKeyboard));
+                                userState.put(chatId, true);
                             }
+                        }
+
+                    } else if (update.message() != null){
+
+                        String tableName = update.message().text();
+
+                        try {
+                            Statement statement = connection.createStatement();
+                            statement.executeUpdate("CREATE TABLE " + tableName + " (cum INTEGER);");
+                            statement.closeOnCompletion();
+                            System.out.println("succ");
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                            e.printStackTrace();
+                        }
+                        userState.put(chatId,false);
+
+                    } else if (update.callbackQuery() != null) {
+                        if (update.callbackQuery().data().equals("cum")) {
+                            SendResponse response = bot.execute(new SendMessage(chatId,"sad"));
                             userState.put(chatId,false);
                         }
-                        //SendResponse valuesResponse = bot.execute(new SendMessage(chatId,"Send me values you want to be added into the table" + "Example:" + "(id INTEGER," + " name STRING)"));
                     }
-
                 }
-
                 return UpdatesListener.CONFIRMED_UPDATES_ALL;
             });
-
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
